a66071baaa6ce1c43833d25f62374a9d39d8ecde
Luwnto <13521760670@163.com>
2019-09-12 17:35:08 +0800

    修改app.js, 移除不存在的控制器

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(templating());
 // 引入路由文件
 const controller = require('./utils/controller');
 // 注册路由控制器
-app.use(controller(['/controllers', '/controllers/home', '/controllers/admin']));
+app.use(controller(['/controllers', '/controllers/home']));
 
 // 开启web服务并监听3000端口
 app.listen(3000);
